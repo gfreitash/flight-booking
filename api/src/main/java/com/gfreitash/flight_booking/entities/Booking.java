d7ea4c184771f84d9fcf128816142a01894bfc5b
Gabriel Freitas <gfreitash@gmail.com>
2023-01-19 14:39:21 -0300

    Shipped changes that should be in the last commit

--- api/src/main/java/com/gfreitash/flight_booking/entities/Booking.java
+++ api/src/main/java/com/gfreitash/flight_booking/entities/Booking.java
@@ -3,7 +3,6 @@ package com.gfreitash.flight_booking.entities;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,6 +10,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
 @Getter
@@ -20,14 +20,49 @@ import java.io.Serializable;
 public class Booking {
     @EmbeddedId
     private BookingId id;
+    @NotNull
+    private Integer ticket;
+
+    @NotNull
+    private Integer seatRow;
+    @NotNull
+    private Integer seatColumn;
 
     @Embeddable
     @Getter
     @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class BookingId implements Serializable {
         @NotNull
         private Integer flightId;
         @NotNull
         private Integer passengerId;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            BookingId bookingId = (BookingId) o;
+            return flightId.equals(bookingId.flightId) && passengerId.equals(bookingId.passengerId);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(flightId, passengerId);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Booking booking = (Booking) o;
+        return id.equals(booking.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
--- api/src/main/java/com/gfreitash/flight_booking/entities/Passenger.java
+++ api/src/main/java/com/gfreitash/flight_booking/entities/Passenger.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
 @Getter
@@ -30,4 +31,16 @@ public class Passenger {
     @NotNull
     private LocalDate birthDate;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Passenger passenger = (Passenger) o;
+        return id.equals(passenger.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
