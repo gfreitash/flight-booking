a3174e53f8515b634ea143a8274aa95fd36eeed8
Gabriel Freitas <gfreitash@gmail.com>
2023-05-09 23:06:49 -0300

    Modified Role dto's to use the @DtoMapper annotation to generate mappers.

--- api/src/main/java/com/gfreitash/flight_booking/services/RoleService.java
+++ api/src/main/java/com/gfreitash/flight_booking/services/RoleService.java
@@ -1,9 +1,13 @@
 package com.gfreitash.flight_booking.services;
 
+import com.gfreitash.flight_booking.config.ValidationsConfig;
+import com.gfreitash.flight_booking.exceptions.RoleDoesNotExistException;
+import com.gfreitash.flight_booking.repositories.RoleRepository;
 import com.gfreitash.flight_booking.services.dto.input.RoleInputDTO;
 import com.gfreitash.flight_booking.services.dto.output.RoleOutputDTO;
-import com.gfreitash.flight_booking.entities.Role;
-import com.gfreitash.flight_booking.repositories.RoleRepository;
+import com.gfreitash.flight_booking.services.dto.mappers.RoleInputDTOMapper;
+import com.gfreitash.flight_booking.services.dto.mappers.RoleOutputDTOMapper;
+import com.gfreitash.flight_booking.services.validations.SpecificationValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,38 +21,43 @@ import java.util.Optional;
 public class RoleService {
 
     private final RoleRepository roleRepository;
+    private final ValidationsConfig validationsConfig;
+    private final RoleInputDTOMapper roleInputDTOMapper;
+    private final RoleOutputDTOMapper roleOutputDTOMapper;
 
     public RoleOutputDTO saveRole(RoleInputDTO role) {
-        var parentRole = role.parentRole() != null ? roleRepository.findByName(role.parentRole()).orElse(null) : null;
-        var savedRole = roleRepository.save(Role.builder()
-                .name(role.name())
-                .parentRole(parentRole)
-                .build());
-        return new RoleOutputDTO(savedRole);
+        List<SpecificationValidator<RoleInputDTO>> validators = validationsConfig.inputSpecifications(RoleInputDTO.class);
+        validators.forEach(validator -> validator.validate(role));
+
+        var savedRole = roleRepository.save(roleInputDTOMapper.toEntity(role));
+        return roleOutputDTOMapper.toDto(savedRole);
     }
 
     public RoleOutputDTO updateRole(String id, RoleInputDTO role) {
-        var parentRole = role.parentRole() != null ? roleRepository.findByName(role.parentRole()).orElse(null) : null;
-        var roleToUpdate = roleRepository.findById(Integer.parseInt(id)).orElseThrow();
+        List<SpecificationValidator<RoleInputDTO>> validators = validationsConfig.inputSpecifications(RoleInputDTO.class);
+        validators.forEach(validator -> validator.validate(role));
+
+        var roleToUpdate = roleRepository.findById(Integer.parseInt(id)).orElseThrow(()->new RoleDoesNotExistException("Role does not exist"));
         roleToUpdate.setName(role.name());
-        roleToUpdate.setParentRole(parentRole);
-        return new RoleOutputDTO(roleRepository.save(roleToUpdate));
+        roleToUpdate.setParentRole(roleRepository.findByName(role.parentRole()).orElse(null));
+
+        return roleOutputDTOMapper.toDto(roleRepository.save(roleToUpdate));
     }
 
     public Optional<RoleOutputDTO> getRoleById(String id) {
-        return roleRepository.findById(Integer.parseInt(id)).map(RoleOutputDTO::new);
+        return roleRepository.findById(Integer.parseInt(id)).map(roleOutputDTOMapper::toDto);
     }
 
     public Optional<RoleOutputDTO> getRoleByName(String name) {
-        return roleRepository.findByName(name).map(RoleOutputDTO::new);
+        return roleRepository.findByName(name).map(roleOutputDTOMapper::toDto);
     }
 
     public List<RoleOutputDTO> getAllRoles() {
-        return roleRepository.findAll().stream().map(RoleOutputDTO::new).toList();
+        return roleRepository.findAll().stream().map(roleOutputDTOMapper::toDto).toList();
     }
 
     public Page<RoleOutputDTO> getAllRoles(Pageable pagination) {
-        return roleRepository.findAll(pagination).map(RoleOutputDTO::new);
+        return roleRepository.findAll(pagination).map(roleOutputDTOMapper::toDto);
     }
 
     public void deleteRole(Integer id) {
--- api/src/main/java/com/gfreitash/flight_booking/services/dto/output/RoleOutputDTO.java
+++ api/src/main/java/com/gfreitash/flight_booking/services/dto/output/RoleOutputDTO.java
@@ -1,6 +1,10 @@
 package com.gfreitash.flight_booking.services.dto.output;
 
+import com.gfreitash.dto_mapper_processor.DtoMapper;
+import com.gfreitash.flight_booking.config.ApplicationConfig;
 import com.gfreitash.flight_booking.entities.Role;
+import com.gfreitash.flight_booking.services.dto.mappers.RoleMapper;
+import com.gfreitash.flight_booking.services.dto.mappers.RoleOutputDTOMapper;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
@@ -8,6 +12,14 @@ import org.springframework.hateoas.server.core.Relation;
 
 @Builder
 @Relation(collectionRelation = "roles", itemRelation = "role")
+@DtoMapper(
+        entity = Role.class,
+        componentModel = "spring",
+        implementationPackage = ApplicationConfig.MAPPERS_PACKAGE,
+        uses = {RoleMapper.class}
+)
+
+
 public record RoleOutputDTO(
         @NotNull Integer id,
 
@@ -16,7 +28,10 @@ public record RoleOutputDTO(
         String parentRole
 ) {
 
-
+    /**
+     * @deprecated Use {@link RoleOutputDTOMapper} instead
+     */
+    @Deprecated(since = "Use RoleOutputDTOMapper instead")
     public RoleOutputDTO(Role role) {
         this(
                 role.getId(),
